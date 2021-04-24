dbf43332dd6ec27c3fdb03a9c27dcce00073dd4c
rstyro <1006059906@qq.com>
2021-04-24 15:57:49 +0800

    单点redis

--- springboot-redis/springboot-redis-standalone/src/main/java/top/lrshuai/SpringbootRedisApplication.java
+++ springboot-redis/springboot-redis-standalone/src/main/java/top/lrshuai/SpringbootRedisApplication.java
@@ -3,6 +3,10 @@ package top.lrshuai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * 不用启动，看test 包里面的测试类
+ */
 @SpringBootApplication
 public class SpringbootRedisApplication {
 
