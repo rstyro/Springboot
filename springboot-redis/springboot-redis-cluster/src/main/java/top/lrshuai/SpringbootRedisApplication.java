b9cb738828b0a8e68b97608a3a14828f335ec4f6
rstyro <1006059906@qq.com>
2021-04-25 21:34:43 +0800

    redis 集群配置

---
+++ springboot-redis/springboot-redis-cluster/src/main/java/top/lrshuai/SpringbootRedisApplication.java
@@ -0,0 +1,15 @@
+package top.lrshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 看test 那个包，demo运行那个就行
+ */
+@SpringBootApplication
+public class SpringbootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisApplication.class, args);
+	}
+}
