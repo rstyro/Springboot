d2171af3b253fff3d4dcaff750f8cf2f8d744251
lurishuai <lurishuai@aspirecn.com>
2025-11-25 15:24:55 +0800

    添加示例日志配置文件

--- springboot-log/src/main/java/top/lrshuai/helloword/Application.java
+++ springboot-log/src/main/java/top/lrshuai/helloword/Application.java
@@ -1,12 +1,21 @@
 package top.lrshuai.helloword;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	private static Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		String name="rstyro";
+		log.debug("------------debug--------------{}", name);
+		log.info("------------info--------------{}", name);
+		log.warn("------------warn--------------{}", name);
+		log.error("------------error--------------{}", name);
 	}
 }
